93fa46fd4e956226c19d05bbe1aae5df135ffbd4
Erculescu Rares <erculescur@gmail.com>
2025-03-03 12:43:41 +0200

    seminar 2

---
+++ Seminar2/Seminar2/Angajat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar2
+{
+    public class Angajat
+    {
+        public Angajat(int marca,string nume,string functie)
+        {
+            this.marca = marca;
+            this.nume = nume;
+            this.functie = functie;
+            sef = null;
+        }
+      public int marca { get; set; }
+        public string nume { get; set; }
+        public string functie { get; set; }
+        public Angajat sef { get; internal set; }
+
+        public override string ToString()
+        {
+            return $"#{marca,-3} {nume} ({functie}" + $",{(sef == null ? "-":sef)})";
+
+        }
+
+    }
+}
--- Seminar2/Seminar2/Program.cs
+++ Seminar2/Seminar2/Program.cs
@@ -1,1 +1,18 @@
 ﻿//seminar 2
+using Seminar2;
+
+namespace Semniar2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ion = new Angajat(1, "Ion", "manager");
+            var rares = new Angajat(2, "Rares", "dezvoltator");
+            var alina = new Angajat(3, "Alina", "sef echipa");
+            Console.WriteLine(ion);
+            Console.WriteLine(rares);
+            Console.WriteLine(alina);
+        }
+    }
+}
